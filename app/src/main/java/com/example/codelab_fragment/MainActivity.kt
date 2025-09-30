fccb4cb32fc9665d554541193e339db16f968484
Mike <131933991+privmike@users.noreply.github.com>
2025-09-30 18:20:46 +0700

    page 4

--- app/src/main/java/com/example/codelab_fragment/MainActivity.kt
+++ app/src/main/java/com/example/codelab_fragment/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.codelab_fragment
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,27 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+    val btnFragmentSatu = findViewById<Button>(R.id.btnFragmentSatu)
+    val btnFragmentDua = findViewById<Button>(R.id.btnFragmentDua)
+
+
+        if (savedInstanceState==null){
+            replaceFragment(FSatu())
+        }
+
     }
+
+
+
+    private fun replaceFragment(fragment : Fragment){
+        val fragmentmanaget = supportFragmentManager
+        val fragmenttransaction = fragmentmanaget.beginTransaction()
+
+        fragmenttransaction.replace(R.id.fragment_container,fragment)
+        fragmenttransaction.addToBackStack(null)
+        fragmenttransaction.commit()
+
+    }
+
 }
